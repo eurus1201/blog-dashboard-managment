140adbe2f88f47c19a59a18b0b02b55b3bd3e79e
khodabakhshian.s@gmail.com <khodabakhshian.s@gmail.com>
2024-04-06 00:46:23 +0330

    fixed navbar and sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,38 +1,96 @@
 <template>
-  <aside class="water-blue">
-    <nav class="sidebar">
-      <p class="post">Post</p>
-      <ul class="list-unstyled">
-        <li class="item">
-          <span>All Articles</span>
-        </li>
-        <li class="item">
-          <span>New Article</span>
-        </li>
-      </ul>
-    </nav>
-  </aside>
+  <div>
+    <aside class="sidebarClass bg-primary" :class="{ show: isSidebarOpen }">
+      <div
+        class="nav flex-column nav-pills"
+        id="v-pills-tab"
+        role="tablist"
+        aria-orientation="vertical"
+      >
+        <p class="post">Post</p>
+        <router-link
+          class="nav-link active"
+          :class="{ active: isActive('/allPosts') }"
+          to="/allPosts"
+          >All Articles</router-link
+        >
+        <router-link
+          class="nav-link"
+          :class="{ active: isActive('/newPost') }"
+          to="/newPost"
+          >New Article</router-link
+        >
+      </div>
+    </aside>
+    <button class="sidebar-toggle" @click="toggleSidebar" v-if="isMobile">
+      <span class="navbar-toggler-icon">...</span>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Sidebar",
+  data() {
+    return {
+      isSidebarOpen: false,
+      isMobile: false,
+    };
+  },
+  created() {
+    this.isMobile = window.innerWidth < 768;
+    window.addEventListener("resize", this.updateIsMobile);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.updateIsMobile);
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarOpen = !this.isSidebarOpen;
+      if (this.isMobile) {
+        document.querySelector(".sidebarClass").style.width = this.isSidebarOpen
+          ? "250px"
+          : "0";
+      }
+    },
+    isActive(route) {
+      return this.$route.path === route;
+    },
+    updateIsMobile() {
+      this.isMobile = window.innerWidth < 768;
+    },
+  },
 };
 </script>
 
 <style scoped>
-.water-blue {
-  background-color: #1c7cd5;
+.sidebarClass {
   position: fixed;
   left: 0;
   height: 100vh;
   width: 250px;
-  top: 55px;
+  top:55px;
+  transition: width 0.3s ease;
+  overflow: hidden;
+}
+.show {
+  width: 300px;
+}
+.sidebar-toggle {
+  position: absolute;
+  top:45px;
+  left: 0px;;
+  display: none;
+  z-index: 30000;
+}
+.sidebar-toggle .navbar-toggler-icon {
+  color: #000;
+  font-size: 24px;
 }
 .post {
   width: 45px;
   height: 26px;
-  margin: 10px 185px 16px 20px;
+  margin: 25px 185px 16px 20px;
   font-family: HelveticaNeue;
   font-size: 22px;
   font-weight: normal;
@@ -43,12 +101,28 @@ export default {
   text-align: left;
   color: #fff;
 }
-.item {
-  width: 250px;
-  height: 43px;
-  margin: 16px 0;
-  padding: 11px 132px 11px 34px;
+.nav-link {
   text-align: left;
   color: #fff;
+  transition: background-color 0.3s ease;
+}
+.nav-link:hover {
+  background-color: rgba(255, 255, 255, 0.15);
+}
+.nav-link.active {
+  background-color: rgba(255, 255, 255, 0.15);
+}
+@media (max-width: 767px) {
+  .sidebarClass {
+    width: 0;
+    overflow: hidden;
+    transition: none;
+  }
+  .show {
+    width: 0;
+  }
+  .sidebar-toggle {
+    display: block;
+  }
 }
 </style>
