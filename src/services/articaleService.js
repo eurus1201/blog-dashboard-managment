f9591e397712ee463f185c4d3716fadf05140424
khodabakhshian.s@gmail.com <khodabakhshian.s@gmail.com>
2024-04-07 07:20:00 +0330

    added toast component

--- src/services/articaleService.js
+++ src/services/articaleService.js
@@ -1,6 +1,19 @@
 import withAuth from "./withAuth";
+import Toast from "@/components/Toast.vue";
+import router from "@/router";
 
 const BASE_URL = "https://api.realworld.io/api";
+
+const showToastMessage = (message, status) => {
+  const toast = new Toast({
+    propsData: {
+      message,
+      status,
+    },
+  });
+  toast.$mount();
+  document.body.appendChild(toast.$el);
+};
 
 export const getAllArticles = async () => {
   try {
@@ -8,6 +21,7 @@ export const getAllArticles = async () => {
     return data.articles;
   } catch (error) {
     console.error("Error fetching articles:", error);
+    showToastMessage("Error fetching articles.", "Error");
     return [];
   }
 };
@@ -15,9 +29,10 @@ export const getAllArticles = async () => {
 export const deleteArticle = async (slug) => {
   try {
     await withAuth("delete", `${BASE_URL}/articles/${slug}`);
-    console.log("Article deleted successfully.");
+    showToastMessage("Article deleted successfully.", "Success");
   } catch (error) {
     console.error("Error deleting article:", error);
+    showToastMessage("Error deleting article.", "Error");
     throw error;
   }
 };
@@ -34,9 +49,27 @@ export const createNewArticle = async (title, description, body, tagList) => {
     };
 
     await withAuth("post", `${BASE_URL}/articles`, articleData);
-    console.log("Article created successfully.");
+    showToastMessage("Well done! Article created successfully.", "Success");
+    router.push("/allPosts");
   } catch (error) {
     console.error("Error creating article:", error);
+    showToastMessage("Error creating article.", "Error");
+    throw error;
+  }
+};
+
+export const editArticle = async (slug, updatedBody) => {
+  try {
+    const articleData = {
+      article: {
+        body: updatedBody,
+      },
+    };
+    await withAuth("put", `${BASE_URL}/articles/${slug}`, articleData);
+    showToastMessage("Well done! Article updated successfully.", "Success");
+    router.push("/allPosts");
+  } catch (error) {
+    console.error("Error updating article:", error);
     throw error;
   }
 };
